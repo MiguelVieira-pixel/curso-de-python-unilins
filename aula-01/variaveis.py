edcaa7bd83cf6d26287de0e892da754adb072766
Miguel Vieira <gitprogama@gmail.com>
2026-01-26 21:20:48 -0300

    Add initial Python scripts for arithmetic operations and variable demonstration

---
+++ aula-01/variaveis.py
@@ -0,0 +1,9 @@
+# name = 'Miguel '
+# surname = 'Vieira'
+# complete_name = name + surname
+# print(complete_name)
+# media = 2
+# n1 = n2 = n3 = n4 = 0
+# name, age = 'Miguel', 18
+# estate = True
+# print(f'Meu nome é {name} tenho {age} anos.')
